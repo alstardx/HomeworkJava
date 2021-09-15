443a96f73fe3c1fbfefd5cf22cf7a282b3834568
aleks <alexander_at@inbox.lv>
2021-09-15 10:14:46 +0100

    Homework1

---
+++ Level2/HW1/Participant.java
@@ -0,0 +1,13 @@
+package Lesson0001.HW1;
+
+public interface Participant {
+    String getName();
+
+    boolean isOnDistance();
+
+    void run(int distance);
+
+    void jump(int height);
+
+    void swim(int distance);
+}
